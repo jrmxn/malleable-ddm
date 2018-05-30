72c8014d3ca1f664db13cb1c004ed446b004ed09
mcintosh <j.mcintoshr@gmail.com>
2018-05-30 16:21:12 -0400

    for testing of basic fits

---
+++ test_ddm_run.m
@@ -0,0 +1,52 @@
+clear;
+f_path_data = 'testing.csv';
+for ix_fit_type = [2,3]
+    for ix_sub = 1:21
+        
+        clear sr;
+        subject = sprintf('sub%02d',ix_sub);
+        sr = ddm_def_base;
+        mk = sr.ddm_get_instance('keyr');
+        %%
+        id_model = sr.debi_model(0,'de','bi');
+        id_model(mk.s) = 1;
+        id_model(mk.z) = 1;
+        id_model(mk.a) = 1;
+        id_model(mk.t) = 1;
+        id_model(mk.v) = 1;
+        id_model(mk.st) = 1;
+        id_model(mk.sz) = 1;
+        id_model = sr.debi_model(id_model,'bi','de');
+        
+        id_search = sr.debi_model(0,'de','bi');
+        id_search(mk.s) = 0;
+        id_search(mk.z) = 0;
+        id_search(mk.a) = 1;
+        id_search(mk.t) = 1;
+        id_search(mk.v) = 1;
+        id_search(mk.st) = 1;
+        id_search(mk.sz) = 1;
+        
+        id_search = sr.debi_model(id_search,'bi','de');
+        
+        sr.subject = subject;
+        sr.path_data = f_path_data;
+        
+        
+        
+        sr.ddm_init(id_model,id_search);
+        if ix_fit_type == 1
+            sr.modelclass = 'base_ana';
+            sr.ddm_pdf = @(a,b) sr.ddm_pdf_ana(a,b);
+        elseif ix_fit_type ==2
+            sr.modelclass = 'base_ana_prt';
+            sr.ddm_pdf = @(a,b) sr.ddm_prt_ana(a,b);
+        elseif ix_fit_type==3
+            sr.modelclass = 'base_trm';
+            sr.ddm_pdf = @(a,b) sr.ddm_pdf_trm(a,b,sr.s.dx);
+        end
+        sr.ddm_fit;
+        sr.ddm_fit;
+        sr.ddm_save;
+    end
+end
