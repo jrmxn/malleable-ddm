afd117465816b51be27d57e058ee89d3cf7dd244
mcintosh <j.mcintoshr@gmail.com>
2018-05-30 16:20:32 -0400

    updated for new ddm_def_conflict

--- test_ddm_run_conflict.m
+++ test_ddm_run_conflict.m
@@ -1,10 +1,10 @@
 clear;
 
 
-niter = 1000;
 f_path_data = 'testing.csv';
-
-for ix_sub = 1%:21
+n_subs = 21;
+f_savepath = cell(1,n_subs);
+for ix_sub = 1:n_subs
     subject = sprintf('sub%02d',ix_sub);
     clear sr;
     sr = ddm_def_conflict;
@@ -12,6 +12,7 @@ for ix_sub = 1%:21
     %%
     id_model = sr.debi_model(0,'de','bi');
     id_model(mk.s) = 1;
+    id_model(mk.z) = 1;
     id_model(mk.a) = 1;
     id_model(mk.t) = 1;
     id_model(mk.v) = 1;
@@ -19,7 +20,6 @@ for ix_sub = 1%:21
     id_model = sr.debi_model(id_model,'bi','de');
     
     id_search = sr.debi_model(0,'de','bi');
-    id_search(mk.s) = 0;
     id_search(mk.a) = 1;
     id_search(mk.t) = 1;
     id_search(mk.v) = 1;
@@ -30,32 +30,33 @@ for ix_sub = 1%:21
     sr.path_data = f_path_data;
     
     sr.ddm_init(id_model,id_search);
-    % sr.opt.MaxIter = niter;
+    sr.ddm_pdf = @(a,b) sr.ddm_prt_ana(a,b);
     sr.ddm_fit;
     sr.ddm_fit;
-    f_savepath = sr.ddm_save;
+    sr.ddm_save;
     px = sr.fit(end).p;px.c = 1;
     %make 100 draws from the pdf
-    sr.ddm_data_draw(px,100);
+%     sr.ddm_data_draw(px,100);
     %% Test to use previous model as start point
     % clearvars -except f_savepath;
     % sr = load(f_savepath);sr = sr.obj;
     mk = sr.ddm_get_instance('keyr');
     id_model = sr.debi_model(sr.id_model,'de','bi');
-    id_model(mk.xb) = 1;
+    id_model(mk.zc) = 1;
     id_model(mk.b) = 1;
     id_model = sr.debi_model(id_model,'bi','de');
     id_search = sr.debi_model(sr.id_search,'de','bi');
-    id_search(mk.xb) = 1;
+    id_search(mk.zc) = 1;
     id_search(mk.b) = 1;
     id_search = sr.debi_model(id_search,'bi','de');
     %re-initiate the model
     sr.ddm_init(id_model,id_search);
-    % sr.opt.MaxIter = niter;
+    sr.ddm_pdf = @(a,b) sr.ddm_pdf_trm(a,b,sr.s.dx);
     sr.ddm_fit;
     sr.ddm_fit;
-    f_savepath = sr.ddm_save;
-    %%
+    f_savepath{ix_sub} = sr.ddm_save;
+end
+for ix_sub = 1:n_subs
     clearvars -except f_savepath;
     sr = load(f_savepath);sr = sr.obj;
     tic;
