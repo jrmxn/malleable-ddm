354c77b3bcc82b12fa3d3db98cde5fd1761c2d89
mcintosh <j.mcintoshr@gmail.com>
2018-05-31 13:48:56 -0400

    .

--- testing/test_ddm_run_sz_base.m
+++ testing/test_ddm_run_sz_base.m
@@ -1,43 +1,36 @@
 clear;
-sub_cell{1} = 'AHLAJ';sz(1) = true;
-sub_cell{2} = 'QQYAK';sz(2) = false;
+addpath('/hdd/Local/gitprojects/ext-ddm/');
+[sub_data_struc,sub_cell,sz] = base_def_subjects('subjects','ALL');
+path_data = fullfile(pwd,'testing_sz.csv');
+%%
 for ix_sub = 1:length(sub_cell)
     
     clear sr;
-    sr = ddm_def_sz_base;
+    sr = ddm_def_sz;
     sr.subject = sub_cell{ix_sub};
-%     sr.get_data;% in this case our instance is dependent on data so get it first.
+    sr.path_data = path_data;
+    %     sr.get_data;% in this case our instance is dependent on data so get it first.
     mk = sr.ddm_get_instance('keyr');
-    %%
-    id_model = sr.debi_model(0,'de','bi');
-    id_model(mk.s) = 1;
-    id_model(mk.z) = 1;
-    id_model(mk.a) = 1;
-    id_model(mk.t) = 1;
-    id_model(mk.vm1) = 1;id_model(mk.vm2) = 1;id_model(mk.vm3) = 1;id_model(mk.vm4) = 1;id_model(mk.vm5) = 1;
-    id_model(mk.vz) = 1;
-    id_model(mk.vp1) = 1;id_model(mk.vp2) = 1;id_model(mk.vp3) = 1;id_model(mk.vp4) = 1;id_model(mk.vp5) = 1;
-    id_model(mk.st) = 1;
-    id_model = sr.debi_model(id_model,'bi','de');
-    
+    %% model def
     id_search = sr.debi_model(0,'de','bi');
-    id_search(mk.s) = 0;
-    id_search(mk.z) = 0;
     id_search(mk.a) = 1;
     id_search(mk.t) = 1;
     id_search(mk.vm3) = 1;id_search(mk.vm4) = 1;id_search(mk.vm5) = 1;
     id_search(mk.vz) = 1;
     id_search(mk.vp3) = 1;id_search(mk.vp4) = 1;id_search(mk.vp5) = 1;
-    if not(sz)
+    if contains(sr.subject,sz)
         id_search(mk.vm1) = 1;id_search(mk.vm2) = 1;
         id_search(mk.vp1) = 1;id_search(mk.vp2) = 1;
     end
     id_search(mk.st) = 1;
+    id_search_de = sr.debi_model(id_search,'bi','de');
     
-    id_search = sr.debi_model(id_search,'bi','de');
-    
-    sr.ddm_init(id_model,id_search);
-    
+    id_model = id_search;
+    id_model(mk.s) = 1;
+    id_model(mk.z) = 1;
+    id_model_de = sr.debi_model(id_model,'bi','de');
+    %%
+    sr.ddm_init(id_model_de,id_search_de);
     sr.ddm_pdf = @(a,b) sr.ddm_prt_ana(a,b);
     
     sr.ddm_fit;
