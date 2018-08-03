f9a3f18bb88b6e57a129953ddad2f614cf81fc82
jrmxn <j.mcintosh@columbia.edu>
2018-08-03 12:53:21 -0400

    fixed a comment

--- ddm_def.m
+++ ddm_def.m
@@ -754,7 +754,7 @@ classdef ddm_def < matlab.mixin.Copyable%instead of handle
             end
             %
             if isnan(ll_app),ll_app=inf;end
-            k = obj.s.fit_n;%number of free params + 1 for fixed noise
+            k = obj.s.fit_n;%number of free params
             n = sum(not(isnan(p_RT_and_accuracy)));
             %
             bic_app = log(n)*k-2*ll_app;
