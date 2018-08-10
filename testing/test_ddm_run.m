dc560c333ed960a62cea64175ef0d0771b6f4869
jrmxn <j.mcintosh@columbia.edu>
2018-08-10 17:44:23 -0400

    updated

--- testing/test_ddm_run.m
+++ testing/test_ddm_run.m
@@ -1,14 +1,18 @@
+% Simplest demonstration of ddm_def
+% Define the simplest inheriting function ddm_def_base.m
+%%
 clear;
 f_path_data = 'testing.csv';
 addpath('..');
-for ix_fit_type = [1,2,3]
-    for ix_sub = 1:3
+for ix_fit_type = [1:3]
+    for ix_sub = 1%:3
         
         clear sr;
         sr = ddm_def_base;
         sr.subject = sprintf('sub%02d',ix_sub);
+        sr.path_data = fullfile('testing.csv');
         mk = sr.ddm_get_instance('keyr');
-        %%
+        %% Define the model parameters included in the model
         id_model = sr.debi_model(0,'de','bi');
         id_model(mk.s) = 1;
         id_model(mk.z) = 1;
@@ -20,30 +24,53 @@ for ix_fit_type = [1,2,3]
 %         id_model(mk.sv) = 1;
         id_model_de = sr.debi_model(id_model,'bi','de');
         
+        %% Define the model parameters to fit
         id_search = sr.debi_model(0,'de','bi');
-        id_search(mk.s) = 0;
-        id_search(mk.z) = 0;
-        id_search(mk.a) = 1;
-        id_search(mk.t) = 1;
-        id_search(mk.v) = 1;
-        id_search(mk.st) = 1;
-        id_search(mk.sz) = 1;
-%         id_search(mk.sv) = 1;
-        
+        id_search(mk.s) = 0;%noise fixed at s=1, do not optimise
+        id_search(mk.z) = 0;%starting point fixed 
+        id_search(mk.a) = 1;%fit the threshold
+        id_search(mk.t) = 1;%fit the non-decision time
+        id_search(mk.v) = 1;%fit the drift
+        id_search(mk.st) = 1;%fit the noise in non-decision time
+        id_search(mk.sz) = 1;%fit noise in starting point
+%         id_search(mk.sv) = 1;%fit noise in drift (slow)
         id_search_de = sr.debi_model(id_search,'bi','de');
 
+        % Initialise the fit
         sr.ddm_init(id_model_de,id_search_de);
-        if ix_fit_type == 2
-            sr.modelclass = 'base_ana';
-            sr.ddm_pdf = @(a,b) sr.ddm_pdf_ana(a,b);
-        elseif ix_fit_type ==1
-            sr.modelclass = 'base_ana_prt';
+        
+        % Set the method used to calculate the likelihood:
+        % In reality you would choose one of these methods, ddm_pdf_ana
+        % (converted from Navaro and Fuss and HDDM) if you have a DDM
+        % (including if you have noise non-decision time, noise in drift
+        % and noise in starting point).
+        % For more complex models you would use ddm_pdf_trm which defines
+        % the DDM with a transition matrix. For specific cases (and to
+        % check correct specification of transition matrix), you may want
+        % to compute with a brute force method (ddm_prt_ana) - not really
+        % intended for optimisation.
+        if ix_fit_type ==1
+            %the HDDM method
+            sr.modelclass = 'base_ana_prt';%just a name for the folder
             sr.ddm_pdf = @(a,b) sr.ddm_prt_ana(a,b);
-        elseif ix_fit_type==3
-            sr.modelclass = 'base_trm';
+        elseif ix_fit_type==2
+            %the transition matrix method
+            sr.modelclass = 'base_trm';%just a name for the folder
+            sr.s.dx = 0.025;%speed things up a bit (not as accurate)
             sr.ddm_pdf = @(a,b) sr.ddm_pdf_trm(a,b,sr.s.dx);
+        elseif ix_fit_type==3
+            %brute force method
+            sr.modelclass = 'base_bru';%just a name for the folder
+            sr.s.nits = 10e3;
+            sr.ddm_pdf = @(a,b) sr.ddm_pdf_bru(a,b,sr.s.nits);
+        elseif ix_fit_type == 4
+            % buggy - needs fixing
+            %the HDDM method used to evaluate the entire PDF (a bit slower,
+            %mainly used for plotting)
+            sr.modelclass = 'base_ana';%just a name for the folder
+            sr.ddm_pdf = @(a,b) sr.ddm_pdf_ana(a,b);
         end
-        sr.ddm_fit;
+        
         sr.ddm_fit;
         sr.ddm_save;
     end
--- testing/test_ddm_run_conflict.m
+++ testing/test_ddm_run_conflict.m
@@ -1,28 +1,34 @@
-% function test_ddm_run_conflict
-n_subs = 21;%%
-vec_sub = 1:5
-% :n_subs;
-% clearvars -except vec_sub vec_sub_oi;
-% sub_data_struc = base_def_subjects('subjects','ALL');
-which_ddm_def = which('ddm_def');
-% if isempty(which_ddm_def)
-addpath('..');
-% addpath(fullfile(libgit,'dm_ext_ddm'));
-% end
-path_data = fullfile(pwd,'testing.csv');
-% path_data = fullfile(pwd,'local_data','20180531_simon_beh.csv');
-
-
-n_subs = 21;
+function test_ddm_run_conflict(model_type,varargin)
+%% Example function for fitting the SE-DDM
+% example use:
+% test_ddm_run_conflict('SE-DDM')
+%%
+d.vec_sub = 1:3;
+v = inputParser;
+addOptional(v,'vec_sub',d.vec_sub);
+parse(v,varargin{:})
+v = v.Results;
+d = [];clear d;
+%%
+addpath(fullfile('..','auxf'));
+addpath(fullfile('..'));
+path_data = fullfile('testing.csv');
+n_fits = 5;
+vec_sub = v.vec_sub;
 %% Fit a basic DDM to get in the ballpark
+sr_loaded_fit = cell(1,length(vec_sub));
 for ix_sub = 1:length(vec_sub)
     clear sr;
-    rng(ix_sub);
+    rng(vec_sub(ix_sub));
+    %Set sr to be a ddm_def_conflict object:
     sr = ddm_def_conflict;
-    sr.subject = sprintf('sub%02d',ix_sub);
+    sr.subject = sprintf('sub%02d',vec_sub(ix_sub));
     sr.path_data = path_data;
+    sr.info.description = 'seed';
+    sr.extra_string = '_seed';
+    % get the key to index the model parameters
     mk = sr.ddm_get_instance('keyr');
-    %
+    % set the search parameters for the model
     id_search = sr.debi_model(0,'de','bi');
     id_search(mk.a) = 1;
     id_search(mk.t) = 1;
@@ -30,6 +36,8 @@ for ix_sub = 1:length(vec_sub)
     id_search(mk.st) = 1;
     id_search_de = sr.debi_model(id_search,'bi','de');
     
+    %set the model parameters (the same as the search parameters plus two
+    %more)
     id_model = id_search;
     id_model(mk.s) = 1;
     id_model(mk.z) = 1;
@@ -38,152 +46,94 @@ for ix_sub = 1:length(vec_sub)
     sr.ddm_init(id_model_de,id_search_de);
     
     if not(exist(sr.ddm_get_save_path,'file')==2)
-        sr.ddm_pdf = @(a,b) sr.ddm_pdf_ana(a,b);
+        sr.ddm_pdf = @(a,b) sr.ddm_prt_ana(a,b);
         sr.s.reinit = true;
-        sr.opt.MaxIter = 2500;
-        sr.ddm_fit;
-        
-%         sr.ddm_pdf = @(a,b) sr.ddm_pdf_trm(a,b,sr.s.dx);
-%         sr.s.reinit = false;
-%         sr.ddm_fit;
-sr.ddm_save;
-    end
-    
-end
-%% fit bias only
-for ix_sub = 1:length(vec_sub)
-    clear sr;
-    rng(ix_sub);
-    sr_load = ddm_def_conflict;
-    sr_load.subject = sprintf('sub%02d',ix_sub);
-    sr_load.path_data = path_data;
-    mk = sr_load.ddm_get_instance('keyr');
-    %
-    id_search = sr_load.debi_model(0,'de','bi');
-    id_search(mk.a) = 1;
-    id_search(mk.t) = 1;
-    id_search(mk.v) = 1;
-    id_search(mk.st) = 1;
-    id_search_de = sr_load.debi_model(id_search,'bi','de');
-    
-    id_model = id_search;
-    id_model(mk.s) = 1;
-    id_model(mk.z) = 1;
-    id_model_de = sr_load.debi_model(id_model,'bi','de');
-    
-    sr_load.ddm_init(id_model_de,id_search_de);
-    
-    sr = load(sr_load.ddm_get_save_path);
-    sr = sr.obj;clear sr_load;
-    
-    id_search(mk.zc) = 1;
-    id_search_de = sr.debi_model(id_search,'bi','de');
-    id_model(mk.zc) = 1;
-    id_model_de = sr.debi_model(id_model,'bi','de');
-    sr.ddm_init(id_model_de,id_search_de);
-    sr.ddm_pdf = @(a,b) sr.ddm_pdf_trm(a,b,sr.s.dx);
-    
-    if not(exist(sr.ddm_get_save_path,'file')==2)
-        sr.s.reinit = false;
-        sr.opt.MaxIter = sr.opt.MaxIter*3;
-        sr.ddm_fit;
         sr.ddm_fit;
         sr.ddm_save;
+    else
+        sr = load(sr.ddm_get_save_path);
+        sr = sr.obj;
     end
-    
-end
-%% fit attention only
-for ix_sub = 1:length(vec_sub)
-    clear sr;
-    rng(ix_sub);
-    sr_load = ddm_def_conflict;
-    sr_load.subject = sprintf('sub%02d',ix_sub);
-    sr_load.path_data = path_data;
-    mk = sr_load.ddm_get_instance('keyr');
-    %
-    id_search = sr_load.debi_model(0,'de','bi');
-    id_search(mk.a) = 1;
-    id_search(mk.t) = 1;
-    id_search(mk.v) = 1;
-    id_search(mk.st) = 1;
-    id_search_de = sr_load.debi_model(id_search,'bi','de');
-    
-    id_model = id_search;
-    id_model(mk.s) = 1;
-    id_model(mk.z) = 1;
-    id_model_de = sr_load.debi_model(id_model,'bi','de');
-    
-    sr_load.ddm_init(id_model_de,id_search_de);
-    
-    sr = load(sr_load.ddm_get_save_path);
-    sr = sr.obj;clear sr_load;
-    
-    id_search(mk.b) = 1;
-    id_search_de = sr.debi_model(id_search,'bi','de');
-    id_model(mk.b) = 1;
-    id_model_de = sr.debi_model(id_model,'bi','de');
-    sr.ddm_init(id_model_de,id_search_de);
-    sr.ddm_pdf = @(a,b) sr.ddm_pdf_trm(a,b,sr.s.dx);
-    
-    if not(exist(sr.ddm_get_save_path,'file')==2)
-        sr.s.reinit = false;
-        sr.opt.MaxIter = sr.opt.MaxIter*3;
-        sr.ddm_fit;
-        sr.ddm_fit;
-        sr.ddm_save;
-    end
-    
+    sr_loaded_fit{1,vec_sub(ix_sub)} = sr.fit(end);
 end
 %% full model
+fprintf('Fitting full model...\n');
 for ix_sub = 1:length(vec_sub)
     clear sr;
-    rng(ix_sub);
-    sr_load = ddm_def_conflict;
-    sr_load.subject = sprintf('sub%02d',ix_sub);
-    sr_load.path_data = path_data;
-    mk = sr_load.ddm_get_instance('keyr');
+    rng(vec_sub(ix_sub));r = randi(1e4);
+    sr = ddm_def_conflict;
+    sr.subject = sprintf('sub%02d',vec_sub(ix_sub));
+    disp(sr.subject);
+    sr.path_data = path_data;
+    sr.info.description = model_type;
+    mk = sr.ddm_get_instance('keyr');
     %
-    id_search = sr_load.debi_model(0,'de','bi');
+    id_search = sr.debi_model(0,'de','bi');
     id_search(mk.a) = 1;
     id_search(mk.t) = 1;
     id_search(mk.v) = 1;
     id_search(mk.st) = 1;
-    id_search_de = sr_load.debi_model(id_search,'bi','de');
+    
+    if strcmpi(model_type,'SE-DDM')
+        id_search(mk.zc) = 1;
+        id_search(mk.b) = 1;
+    elseif strcmpi(model_type,'attention')
+        id_search(mk.b) = 1;
+    elseif strcmpi(model_type,'bias')
+        id_search(mk.zc) = 1;
+    elseif strcmpi(model_type,'DDM')
+    else
+        error('Wrong model type');
+    end
+    
+    id_search_de = sr.debi_model(id_search,'bi','de');
     
     id_model = id_search;
     id_model(mk.s) = 1;
     id_model(mk.z) = 1;
-    id_model_de = sr_load.debi_model(id_model,'bi','de');
-    
-    sr_load.ddm_init(id_model_de,id_search_de);
-    
-    sr = load(sr_load.ddm_get_save_path);
-    sr = sr.obj;clear sr_load;
-    
-    id_search(mk.zc) = 1;
-    id_search(mk.b) = 1;
-    id_search_de = sr.debi_model(id_search,'bi','de');
-    id_model(mk.zc) = 1;
-    id_model(mk.b) = 1;
     id_model_de = sr.debi_model(id_model,'bi','de');
+    
     sr.ddm_init(id_model_de,id_search_de);
+    
+    %now that we are actually fitting models that potentially have non
+    %standard DDM terms, change to the transition matrix approach:
     sr.ddm_pdf = @(a,b) sr.ddm_pdf_trm(a,b,sr.s.dx);
     
-    if not(exist(sr.ddm_get_save_path,'file')==2)
-        sr.s.reinit = false;
-        sr.opt.MaxIter = sr.opt.MaxIter*3;
-        sr.ddm_fit;
+    %this is just for resuming
+    if (exist(sr.ddm_get_save_path,'file')==2)
+        sr_load = load(sr.ddm_get_save_path);
+        sr_load = sr_load.obj;
+        n_done = length(sr_load.fit);
+        [~,best_ix] = min(cellfun(@(x) x.nll,{sr_load.fit}));
+        best_fit = sr_load.fit(best_ix);
+        clear sr;
+        sr = sr_load;
+        clear sr_load;
+    else
+        n_done = 0;
+    end
+    
+    for ix_fit = n_done+1:n_fits
+        fprintf('Fit index: %d\n',ix_fit)
+        rng(vec_sub(ix_sub) + r*(ix_fit-1));
+        if ix_fit == 1
+            % for the first fit use the optimum we had found with the
+            % simple fit
+            sr.s.reinit = false;
+            sr.ddm_fit_init(sr_loaded_fit{1,vec_sub(ix_sub)}.p);
+        elseif (ix_fit == n_fits)
+            % for the last fit, take the best so far and repeat at high
+            % resolution:
+            sr.s.reinit = false;
+            sr.ddm_fit_init(best_fit.p);
+            sr.s.dx = 0.005;
+            sr.opt.dodoublefit = false;
+        else
+            % in general, just redraw the initialisation parameters
+            sr.s.reinit = true;
+        end
         sr.ddm_fit;
         sr.ddm_save;
     end
-    
 end
-% for ix_sub = 1:length(vec_sub)
-%     clearvars -except f_savepath;
-%     sr = load(f_savepath);sr = sr.obj;
-%     tic;
-%     sr.ddm_mcmc('mccount',100e3);
-%     toc;
-%     sr.ddm_save;
-% end
-% rmpath(fullfile(libgit,'dm_ext_ddm'));
+end
